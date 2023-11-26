69fe83bdc04b0ed85941fc1dca52f2d124b0426a
AswinPKumar01 <aswinpkumar03@gmail.com>
2023-11-26 23:59:57 +0530

    path update

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,10 @@ import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
